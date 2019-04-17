b7dd6300d8003ba946b48eb2983b0173ea148425
Randall Reed, Jr <randallreedjr@gmail.com>
2019-04-17 00:47:38 -0400

    Initial commit

---
+++ big_five_results_poster.rb
@@ -0,0 +1,8 @@
+class BigFiveResultsPoster
+  def initialize(results, email)
+  end
+
+  def post
+
+  end
+end
---
+++ big_five_results_text_serializer.rb
@@ -0,0 +1,7 @@
+class BigFiveResultsTextSerializer
+  def initialize(text)
+  end
+
+  def to_h
+  end
+end
